8e21561c590bd93682d81511ec679e82f7e2858e
Andy Tough <andy@andytough.com>
2025-04-23 15:16:21 +0100

    Filter out non human entities

--- js/script.js
+++ js/script.js
@@ -158,7 +158,7 @@ function autocompleteSearch() {
     }
 
     const script = document.createElement('script');
-    script.src = `https://www.wikidata.org/w/api.php?action=wbsearchentities&search=${encodeURIComponent(name)}&language=en&format=json&uselang=en&type=item&continue=0&limit=10&callback=handleAutocompleteResponse&type=item&profile=human`;
+    script.src = `https://www.wikidata.org/w/api.php?action=wbsearchentities&search=${encodeURIComponent(name)}&language=en&format=json&uselang=en&type=item&continue=0&limit=10&callback=handleAutocompleteResponse&filter=instance:Q5`;
     document.head.appendChild(script);
     document.head.removeChild(script);
     
